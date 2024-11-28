8ca80ab2a2009178ed7ad2734861c1d7c06f53c4
Harshu0810 <143718146+Harshu0810@users.noreply.github.com>
2024-11-28 21:48:35 +0530

    Update CustomAlert.jsx

--- src/CustomAlert.jsx
+++ src/CustomAlert.jsx
@@ -1,22 +1,22 @@
-// src/CustomAlert.jsx
-// import React from 'react';
-import PropTypes from 'prop-types';
-import './CustomAlert.css'; // Import custom CSS for alert styling
+// // src/CustomAlert.jsx
+// // import React from 'react';
+// import PropTypes from 'prop-types';
+// import './CustomAlert.css'; // Import custom CSS for alert styling
 
-const CustomAlert = ({ message, onClose }) => {
-  return (
-    <div className="custom-alert">
-      <div className="custom-alert-content">
-        <span className="custom-alert-message">{message}</span>
-        <button className="custom-alert-close" onClick={onClose}>×</button>
-      </div>
-    </div>
-  );
-};
+// const CustomAlert = ({ message, onClose }) => {
+//   return (
+//     <div className="custom-alert">
+//       <div className="custom-alert-content">
+//         <span className="custom-alert-message">{message}</span>
+//         <button className="custom-alert-close" onClick={onClose}>×</button>
+//       </div>
+//     </div>
+//   );
+// };
 
-CustomAlert.propTypes = {
-  message: PropTypes.string.isRequired,
-  onClose: PropTypes.func.isRequired,
-};
+// CustomAlert.propTypes = {
+//   message: PropTypes.string.isRequired,
+//   onClose: PropTypes.func.isRequired,
+// };
 
-export default CustomAlert;
+// export default CustomAlert;
